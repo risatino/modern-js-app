2f54c34e0d60a6b867cd9f54272aa4e8a8cad2f9
Risa Prezzano <charissatolentino@gmail.com>
2017-12-18 18:45:51 +0000

    Created For Loop for Time Zone feature to be placed in models.py file. Use import pytz

--- python/timezone.py
+++ python/timezone.py
@@ -5,5 +5,6 @@ for tz in pytz.all_timezones:
     tz_choices.append((tz.replace('/','_').lower(), tz))
 tz_choices = tuple(tz_choices)
 
-# shortcut one line version
+# shortcut one line version // place in models.py file in macarthur_cms
+
 tz_choices = tuple((tz.replace('/','_').lower(),tz) for tz in pytz.all_timezones)
