dfd3d343c090f521817f24438f3d2f7dd4ea4de5
Risa Prezzano <charissatolentino@gmail.com>
2017-12-28 18:37:21 +0000

    Removed lower case property from pytz code

--- python/timezone.py
+++ python/timezone.py
@@ -8,3 +8,12 @@ tz_choices = tuple(tz_choices)
 # shortcut one line version // place in models.py file in macarthur_cms
 
 tz_choices = tuple((tz.replace('/','_').lower(),tz) for tz in pytz.all_timezones)
+
+# remove lower case and just replace / and _
+
+tz_choices = []
+for tz in pytz.all_timezones:
+    tz_choices.append((tz.replace('/','_'), tz))
+tz_choices = tuple(tz_choices)
+
+tz_choices = tuple((tz.replace('/','_'),tz) for tz in pytz.all_timezones)
