aec607ed73080489a0c7f9a2647b08b592f2ef28
Risa Prezzano <charissatolentino@gmail.com>
2018-01-24 17:18:24 +0000

    Updated xyz-1090, abc-1104, zyx-1100

--- python/timezone.py
+++ python/timezone.py
@@ -29,22 +29,29 @@ tz_choices = tuple((tz.replace('/','_'),tz) for tz in pytz.all_timezones)
 # (PST) Pacific standard time
 # (PDT) Pacific daylight time
 
-# Need to figure out why this doesn't work in apps/organization/models.py
+# Works in apps/organization/models.py
 
-choices=(('GMT', _('(GMT) Greenwich mean time'), 
-        ('EST', _('(EST) Eastern standard time'), 
-        ('EDT', _('(EDT) Eastern daylight time'),
-        ('CST', _('(CST) Central standard time'),
-        ('CDT', _('(CDT) Central daylight time'),
-        ('MST', _('(MST) Mountain standard time'),
-        ('MDT', _('(MDT) Mountain daylight time'),
-        ('PST', _('(PST) Pacific standard time'),
-        ('PDT', _('(PDT) Pacific daylight time'))
-        
-# Tag mapping in apps/articles/management/commands/import_mentions.py
-tag_mapping = { 
-            '40th_anniversary': {'tags': [0]},
+timezone = models.CharField(_("Timezone"),
+        choices=(
+            ('GMT', _('(GMT) Greenwich mean time')), 
+            ('EST', _('(EST) Eastern standard time')), 
+            ('EDT', _('(EDT) Eastern daylight time')),
+            ('CST', _('(CST) Central standard time')),
+            ('CDT', _('(CDT) Central daylight time')),
+            ('MST', _('(MST) Mountain standard time')),
+            ('MDT', _('(MDT) Mountain daylight time')),
+            ('PST', _('(PST) Pacific standard time')),
+            ('PDT', _('(PDT) Pacific daylight time')),
+        ),
+        default='', 
+        blank=True,
+        max_length=256
+    )
+    
+# xyz-1100 - Add a new article type called "40th Anniversary"
+# /apps/articles/models.py line 50 and 250-ish
+# add article type choice ('fam', '40th Anniversary'),
+# add type pair ('fam', 'forty-anniversary'),
             
-# T-1104
-# 'Research & Results' to 'Grantee Research'
+# xyz-1104 - Replaced all 'Research & Results' to 'Grantee Research'
 
