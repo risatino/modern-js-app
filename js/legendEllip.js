cde76e4495a70d1a1556867509c20cfc3f34f6c2
Risa Prezzano <charissatolentino@gmail.com>
2018-05-08 14:17:26 +0000

    include useHTML or it shows up blank. not sure why this is the case

--- js/legendEllip.js
+++ js/legendEllip.js
@@ -5,3 +5,13 @@ labelFormatter: function () {
             return this.name;
           }
         },
+        
+// useHTML: true seems to work better
+            labelFormatter: function () {
+              if(this.name.length > 40) {
+                return this.name.substring(0,37) + '...' 
+              } else {
+                return this.name;
+              }
+            },
+            useHTML: true
